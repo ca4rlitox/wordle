373a9214e0d7ea07fb120295208b4c023de4fa86
ca4rlitox <ch4rl1e.m.c@protonmail.com>
2025-12-28 13:44:26 +0100

    Hecho el metodo haGanadoJugador y el método haTerminadoJuego, completado el menú pero hay que pulirlo y falta desarrollar por completo el método compruebaLetrasAcertadas.

--- wordle/src/wordle/juegoWordle.java
+++ wordle/src/wordle/juegoWordle.java
@@ -6,18 +6,52 @@ import java.util.Scanner;
 public class juegoWordle {
 
 	static String palabraSecreta;
-	static int numIntentosConsumidos;
+	static int numIntentosConsumidos=0;
 	static int numLetrasAdivinadas;
 	static Scanner entrada = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-		generaPalabra();
-		String palabra = entrada.nextLine();
-		System.out.println(compruebaLetrasAcertadas(palabra));
-		System.out.println(palabraSecreta);
+		String palabra="";
+		char opcion;
+		int estadisticasMaquina=0;
+		int estadisticasHumano=0;
 
+		do {
+			System.out.println("Bienvenid@ al juego de Wordle.");
+			System.out.println("El objetivo es descubrir la palabra oculta de 5 letras.");
+			System.out.println("Introduce la palabra de 5 letras:");
+			generaPalabra();
+			do {
+				System.out.print(">");
+				palabra = entrada.nextLine();
+				System.out.println(compruebaLetrasAcertadas(palabra));
+				numIntentosConsumidos++;
+				if (haGanadoJugador(palabra)) {
+					System.out.println("Has ganado!");
+					estadisticasHumano++;
+					System.out.println("Tú: "+estadisticasHumano+" puntos vs Máquina: "+estadisticasMaquina+" punto");
+					break;
+				}
+				else if (haTerminadoJuego(numIntentosConsumidos)) {
+					System.out.println("Has perdido!");
+					estadisticasMaquina++;
+					System.out.println("Tú: "+estadisticasHumano+" puntos vs Máquina: "+estadisticasMaquina+" punto");
+					break;
+				}
+			}while (!palabra.equals(palabraSecreta));
+			numIntentosConsumidos=0;
 
+			System.out.println("¿Deseas jugar otra partida? (S/N)");
+			opcion = entrada.nextLine().charAt(0);
+			if (opcion!='n' && opcion!='N' && opcion!='S' && opcion!='s') {
+				do{
+					System.out.println("Opción elegida no válida. Intentalo de nuevo.");
+					System.out.println("¿Deseas jugar otra partida? (S/N)");
+					opcion = entrada.nextLine().charAt(0);
+				}while (opcion!='n' && opcion!='N' && opcion!='S' && opcion!='s');
+			}
+		} while (opcion != 'n' && opcion !='N');
 	}
 
 	public static void generaPalabra() {
@@ -61,4 +95,10 @@ public class juegoWordle {
 			}
 		return comprobacion;
 	}
+	public static boolean haGanadoJugador (String palabra) {
+        return palabra.equals(palabraSecreta);
+    }
+	public static boolean haTerminadoJuego (int numIntentosConsumidos) {
+        return numIntentosConsumidos == 6;
+    }
 }
