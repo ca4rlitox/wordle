f8bd69b033fbf8553808bb108c848a9ce973af76
ca4rlitox <ch4rl1e.m.c@protonmail.com>
2025-12-30 14:58:35 +0100

    Casi terminado, falta por pulir la condición dentro de las comprobaciones que no haya 2 mismas vocales seguidas, ya que ahora compara en el array las letras que tiene sean vocales o no.

--- wordle/src/wordle/juegoWordle.java
+++ wordle/src/wordle/juegoWordle.java
@@ -23,6 +23,7 @@ public class juegoWordle {
 			System.out.println("Introduce la palabra de 5 letras:");
 			generaPalabra();
 			do {
+				numIntentosConsumidos++;
 				System.out.print(">");
 				palabra = entrada.nextLine();
 				System.out.println(compruebaLetrasAcertadas(palabra));
@@ -38,7 +39,6 @@ public class juegoWordle {
 					System.out.println("Tú: "+estadisticasHumano+" punto/s vs Máquina: "+estadisticasMaquina+" punto/s");
 					break;
 				}
-				numIntentosConsumidos++;
 			}while (!haGanadoJugador(numLetrasAdivinadas) || !haTerminadoJuego(numIntentosConsumidos));
 			numIntentosConsumidos=0;
 			numLetrasAdivinadas=0;
@@ -68,65 +68,91 @@ public class juegoWordle {
 
 	public static String compruebaLetrasAcertadas(String palabraIntroducida) {
 		String comprobacion = "";
-		boolean vocalesSeguidas=false;
-		boolean consonantesSeguidas=false;
+		char[] palabra = new char[palabraSecreta.length()];
 		palabraIntroducida = palabraIntroducida.toUpperCase();
+		int contadorConsonantes=0;
+		int contadorVocalesSeguidas=0;
+		int contadorVocales=0;
 
-		for (int i = 0; i < palabraSecreta.length(); i++) {
 
-			int contadorVocales=0;
-			int contadorVocalesSeguidas=0;
-			int contadorConsonantesSeguidas=0;
+		//Introducimos la palabra en el array para hacer las comprobaciones.
+		for (int i = 0; i < 5; i++) {
+			palabra[i] = palabraIntroducida.charAt(i);
+		}
 
-			if (palabraIntroducida.length() != 5 || palabraIntroducida.charAt(i) < 'A' || palabraIntroducida.charAt(i) > 'Z'
-					|| palabraIntroducida.lastIndexOf('Q') == 4 || palabraIntroducida.lastIndexOf('W') == 4 || palabraIntroducida.lastIndexOf('X') == 4
-					|| palabraIntroducida.charAt(0) == ('A') && palabraIntroducida.charAt(1) == ('A') || palabraIntroducida.charAt(1) == ('A')
-					&& palabraIntroducida.charAt(2) == ('A') || palabraIntroducida.charAt(3) == ('A') && palabraIntroducida.charAt(4) == ('A')||
-					palabraIntroducida.charAt(0) == ('E') && palabraIntroducida.charAt(1) == ('E') || palabraIntroducida.charAt(1) == ('E')
-					&& palabraIntroducida.charAt(2) == ('E') || palabraIntroducida.charAt(3) == ('E') && palabraIntroducida.charAt(4) == ('E')
-					|| palabraIntroducida.charAt(0) == ('I') && palabraIntroducida.charAt(1) == ('I') || palabraIntroducida.charAt(1) == ('I')
-					&& palabraIntroducida.charAt(2) == ('I') || palabraIntroducida.charAt(3) == ('I') && palabraIntroducida.charAt(4) == ('I')
-					|| palabraIntroducida.charAt(0) == ('O') && palabraIntroducida.charAt(1) == ('O') || palabraIntroducida.charAt(1) == ('O')
-					&& palabraIntroducida.charAt(2) == ('O') || palabraIntroducida.charAt(3) == ('O') && palabraIntroducida.charAt(4) == ('O')
-					|| palabraIntroducida.charAt(0) == ('U') && palabraIntroducida.charAt(1) == ('U') || palabraIntroducida.charAt(1) == ('U')
-					&& palabraIntroducida.charAt(2) == ('U') || palabraIntroducida.charAt(3) == ('U') && palabraIntroducida.charAt(4) == ('U')) {
-				do {
-
-					System.out.println("La palabra introducida no cumple los requisitos. Intentalo de nuevo.");
-					palabraIntroducida = entrada.nextLine();
-					palabraIntroducida = palabraIntroducida.toUpperCase();
-
-				} while (palabraIntroducida.length() != 5 || palabraIntroducida.charAt(i) < 'A' || palabraIntroducida.charAt(i) > 'Z'
-						|| palabraIntroducida.lastIndexOf('Q') == 4 || palabraIntroducida.lastIndexOf('W') == 4 || palabraIntroducida.lastIndexOf('X') == 4
-						|| palabraIntroducida.charAt(0) == ('A') && palabraIntroducida.charAt(1) == ('A') || palabraIntroducida.charAt(1) == ('A')
-						&& palabraIntroducida.charAt(2) == ('A') || palabraIntroducida.charAt(3) == ('A') && palabraIntroducida.charAt(4) == ('A')||
-						palabraIntroducida.charAt(0) == ('E') && palabraIntroducida.charAt(1) == ('E') || palabraIntroducida.charAt(1) == ('E')
-						&& palabraIntroducida.charAt(2) == ('E') || palabraIntroducida.charAt(3) == ('E') && palabraIntroducida.charAt(4) == ('E')
-						|| palabraIntroducida.charAt(0) == ('I') && palabraIntroducida.charAt(1) == ('I') || palabraIntroducida.charAt(1) == ('I')
-						&& palabraIntroducida.charAt(2) == ('I') || palabraIntroducida.charAt(3) == ('I') && palabraIntroducida.charAt(4) == ('I')
-						|| palabraIntroducida.charAt(0) == ('O') && palabraIntroducida.charAt(1) == ('O') || palabraIntroducida.charAt(1) == ('O')
-						&& palabraIntroducida.charAt(2) == ('O') || palabraIntroducida.charAt(3) == ('O') && palabraIntroducida.charAt(4) == ('O')
-						|| palabraIntroducida.charAt(0) == ('U') && palabraIntroducida.charAt(1) == ('U') || palabraIntroducida.charAt(1) == ('U')
-						&& palabraIntroducida.charAt(2) == ('U') || palabraIntroducida.charAt(3) == ('U') && palabraIntroducida.charAt(4) == ('U'));
-
+		//Comprobamos si tiene 3 o más consonantes y guardamos el resultado en la variable contadorConsonante.
+		for (int i = 0; i < 5; i++) {
+			if (palabra[i] != 'A' && palabra[i] != 'E' && palabra[i] != 'I' && palabra[i] != 'O' && palabra[i] != 'U') {
+				contadorConsonantes+=1;
+			} else if (contadorConsonantes>2) {
+				break;
+			}
+			else {
+				contadorConsonantes=0;
 			}
 		}
-			for (int i = 0; i < palabraSecreta.length(); i++) {
-				char letraIntroducida = palabraIntroducida.charAt(i);
 
-				if (palabraIntroducida.charAt(i) == palabraSecreta.charAt(i)) {
-					comprobacion += palabraIntroducida.toUpperCase().charAt(i);
-					numLetrasAdivinadas++;
-				} else if (palabraSecreta.indexOf(letraIntroducida) != -1) {
-					comprobacion += palabraIntroducida.toLowerCase().charAt(i);
-				} else {
-					comprobacion += "*";
-				}
+		//Comprobamos en la palabra si hay entre 2 y 3 vocales
+		for (int i = 0; i < 5; i++) {
+			if (palabra[i] == 'A' || palabra[i] == 'E' || palabra[i] == 'I' || palabra[i] == 'O' || palabra[i] == 'U') {
+				contadorVocales+=1;
 			}
-		return comprobacion;
-	}
+		}
+				for (int i = 0; i < palabraSecreta.length(); i++) {
+
+					if (palabraIntroducida.length() != 5 || palabraIntroducida.charAt(i) < 'A' || palabraIntroducida.charAt(i) > 'Z' || contadorVocales>3
+							|| contadorVocales<2|| contadorConsonantes>2 || palabra[4] == 'Q' || palabra[4] == 'W' || palabra[4] == 'X'
+							|| palabra[0] == palabra[1] || palabra[1] == palabra[2] || palabra[2] == palabra[3] || palabra[3] == palabra[4]) {
+						do {
+
+							System.out.println("La palabra introducida no cumple los requisitos. Intentalo de nuevo.");
+							palabraIntroducida = entrada.nextLine();
+							palabraIntroducida = palabraIntroducida.toUpperCase();
+							for (int j = 0; j < 5; j++) {
+								palabra[j] = palabraIntroducida.charAt(j);
+							}
+							// Reiniciamos contadores de consonantes y vocales.
+							contadorConsonantes=0;
+							contadorVocales=0;
+							for (int j = 1; j < 5; j++) {
+								if (palabra[j] != 'A' && palabra[j] != 'E' && palabra[j] != 'I' && palabra[j] != 'O' && palabra[j] != 'U') {
+									contadorConsonantes+=1;
+								} else if (contadorConsonantes>2) {
+									break;
+								} else {
+									contadorConsonantes=0;
+								}
+							}
+
+							//Comprobamos en la palabra si hay entre 2 y 3 vocales
+							for (int j = 0; j < 5; j++) {
+								if (palabra[j] == 'A' || palabra[j] == 'E' || palabra[j] == 'I' || palabra[j] == 'O' || palabra[j] == 'U') {
+									contadorVocales+=1;
+								}
+							}
+
+						} while (palabraIntroducida.length() != 5 || palabraIntroducida.charAt(i) < 'A' || palabraIntroducida.charAt(i) > 'Z' || contadorVocales>3
+								|| contadorVocales<2|| contadorConsonantes>2 || palabra[4] == 'Q' || palabra[4] == 'W' || palabra[4] == 'X'
+								|| palabra[0] == palabra[1] || palabra[1] == palabra[2] || palabra[2] == palabra[3] || palabra[3] == palabra[4]);
+
+					}
+				}
+				for (int i = 0; i < palabraSecreta.length(); i++) {
+					char letraIntroducida = palabraIntroducida.charAt(i);
+
+					if (palabraIntroducida.charAt(i) == palabraSecreta.charAt(i)) {
+						comprobacion += palabraIntroducida.toUpperCase().charAt(i);
+						numLetrasAdivinadas++;
+					} else if (palabraSecreta.indexOf(letraIntroducida) != -1) {
+						comprobacion += palabraIntroducida.toLowerCase().charAt(i);
+					} else {
+						comprobacion += "*";
+					}
+				}
+				return comprobacion;
+			}
 	public static boolean haGanadoJugador (int letras) {
-        return numLetrasAdivinadas==5;
+        return numLetrasAdivinadas>4;
     }
 	public static boolean haTerminadoJuego (int numIntentosConsumidos) {
         return numIntentosConsumidos==6;
